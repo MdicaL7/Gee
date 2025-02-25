c2065b211147526b5925228d89e93902ed6be2db
dicam <507361324@qq.com>
2025-02-25 10:24:39 +0800

    完成错误恢复

--- gee/gee.go
+++ gee/gee.go
@@ -33,6 +33,12 @@ func New() *Engine {
 	return engine
 }
 
+func Default() *Engine {
+	engine := New()
+	engine.Use(Logger(), Recovery())
+	return engine
+}
+
 // Group 创建一个新的RouterGroup
 func (group *RouterGroup) Group(prefix string) *RouterGroup {
 	engine := group.engine
--- main.go
+++ main.go
@@ -2,53 +2,18 @@ package main
 
 import (
 	"Gee/gee"
-	"fmt"
-	"html/template"
 	"net/http"
-	"time"
 )
 
-type student struct {
-	Name string
-	Age  int8
-}
-
-func FormatAsData(t time.Time) string {
-	year, month, day := t.Date()
-	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
-}
-
 func main() {
-	r := gee.New()
-	r.Use(gee.Logger()) // global midlleware
-	r.SetFuncMap(template.FuncMap{
-		"FormatAsDate": FormatAsData,
-	})
-	r.LoadHTMLGlob("templates/*")
-	r.Static("/assets", "./static")
-
-	stu1 := &student{
-		Name: "Geektutu",
-		Age:  20,
-	}
-	stu2 := &student{
-		Name: "Jack",
-		Age:  22,
-	}
+	r := gee.Default()
 	r.GET("/", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "css.tmpl", nil)
+		c.String(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
-	r.GET("/students", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "arr.tmpl", gee.H{
-			"title":  "gee",
-			"stuArr": [2]*student{stu1, stu2},
-		})
-	})
-	r.GET("/date", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "custom_func.tmpl", gee.H{
-			"title": "gee",
-			"now":   time.Date(2020, 11, 10, 0, 0, 0, 0, time.UTC),
-		})
+
+	r.GET("/panic", func(c *gee.Context) {
+		names := []string{"geektutu"}
+		c.String(http.StatusOK, names[100])
 	})
 	r.Run(":9999")
 }
